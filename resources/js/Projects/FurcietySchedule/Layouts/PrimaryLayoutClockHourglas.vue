d434f8de9aa2e59304f690db0a59699565dfe900
LumexDE <email@lukas-weigert.de>
2025-10-19 18:06:29 +0200

    FIX

--- resources/js/Projects/FurcietySchedule/Layouts/PrimaryLayoutClockHourglas.vue
+++ resources/js/Projects/FurcietySchedule/Layouts/PrimaryLayoutClockHourglas.vue
@@ -6,8 +6,8 @@ defineOptions({
     inheritAttrs: false,
 });
 
-import Time from "@/Projects/EF29/Components/CurrentTime.vue";
-//import LogoSVG from "@/Projects/EF29/Assets/images/logoEF27e.svg";
+import Time from "@/Projects/FurcietySchedule/Components/CurrentTime.vue";
+//import LogoSVG from "@/Projects/FurcietySchedule/Assets/images/logoEF27e.svg";
 
 let attrs = reactive(useAttrs());
 
@@ -24,14 +24,14 @@ const usableAttributes = computed(() => {
     <div>
         <!-- animated background -->
         <div
-            class="absolute left-0 top-0 z-0 w-screen h-screen flex items-center justify-center text-center"
+            class="absolute top-0 left-0 z-0 flex items-center justify-center w-screen h-screen text-center"
         >
             <!-- todo: implement -->
         </div>
 
         <Time hourglass="true" />
 
-        <div class="h-screen overflow-auto bg-primary flex flex-col flex-grow">
+        <div class="flex flex-col flex-grow h-screen overflow-auto bg-primary">
             <!-- Main Content -->
             <Transition mode="out-in">
                 <component
