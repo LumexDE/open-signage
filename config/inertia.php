8021b6433fb8f2e9a09585497d771cab499be4ec
LumexDE <email@lukas-weigert.de>
2025-10-19 18:55:51 +0200

    🐛 fix(inertia): disable ssr bundle existence check
    - disable ensure_bundle_exists due to deployment issues

--- config/inertia.php
+++ config/inertia.php
@@ -25,7 +25,7 @@ return [
 
         'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
 
-        'ensure_bundle_exists' => (bool) env('INERTIA_SSR_ENSURE_BUNDLE_EXISTS', true),
+        'ensure_bundle_exists' => (bool) env('INERTIA_SSR_ENSURE_BUNDLE_EXISTS', false),
 
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
 
